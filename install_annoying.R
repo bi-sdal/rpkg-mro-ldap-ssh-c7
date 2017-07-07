aa6a6ab115f1d3ee7d3059876e31599eef3d78ff
Daniel Chen <chendaniely@users.noreply.github.com>
2017-07-07 14:09:19 -0400

    Create install_annoying.R

---
+++ install_annoying.R
@@ -0,0 +1,22 @@
+fury <- c(
+    'tigris'
+)
+
+tryCatch({
+    install.packages(fury)
+}, error = function(e) {
+    stop('R is not being nice')
+    quit(save = "no", status = 1)
+})
+
+print("Finished Installing")
+
+for (p in cran_pkgs){
+    print(sprintf('Loading: %s', p))
+    tryCatch({
+        library(p, character.only = TRUE)
+    }, error = function(e) {
+        stop('R is not being nice')
+        quit(save = "no", status = 1)
+    })
+}
